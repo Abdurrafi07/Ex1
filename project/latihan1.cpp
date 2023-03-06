1e0a951a73f249ee8a6ee48f5e1431503ff517aa
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2023-03-06 12:09:52 +0700

    p

---
+++ project/latihan1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+ // begin
+ // numeric luas,p,l
+ double Luas, p, l;
+ // display 'Masukan Panjangnya = '
+ cout << "Masukan Panjangnya = " ;
+ // accept p
+ cin >> p;
+ // display 'Masukan Lebarnya = '
+ cout << "Masukan Lebarnya = " ;
+ // accept l
+ cin >> l;
+ // compute Luas = p*l
+ Luas = p*l;
+ // display "Luasnya = " + luas
+ cout << "Luasnya = " << Luas;
+ // end   
+}
